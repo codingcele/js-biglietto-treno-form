b28884d39e188169ef4c00ccfd2b9762b1b63a51
codingcele <marcocelegato9@gmail.com>
2022-11-08 16:43:05 +0100

    inizio form

---
+++ main.js
@@ -0,0 +1,27 @@
+
+let km = document.getElementById("km").value;
+
+console.log(km);
+
+/* let age = prompt("How old are you?");
+
+let prcPerKm = 0.21;
+
+let underageDsc = 0.20;
+
+let over65Dsc = 0.40;
+
+if (age < 18) {
+    price = km * prcPerKm * (1 - underageDsc)
+}
+else if (age > 65) {
+    price = km * prcPerKm * (1- over65Dsc)
+}
+else {
+    price = km * prcPerKm
+}
+
+let rndPrice = price.toFixed(2);
+
+
+document.getElementById("getPrice").innerHTML = `Your ticket's price is: ${rndPrice}`; */
